477aee369e6bbd6b12b8b868951f9deb0fc2d605
Kobby Bawuah <bawuah10@hotmail.com>
2021-02-19 22:27:23 -0500

    added second array

--- static/js/insert.js
+++ static/js/insert.js
@@ -1,4 +1,5 @@
 var array = Array();
+var array2 = Array();
 
 function add_element_to_array()
 {
@@ -12,6 +13,16 @@ function add_element_to_array()
     display_array();
 }
 
+function add_element_to_array2() {
+    var text = document.getElementById("text2").value.trim();
+    document.getElementById("text2").value = "";
+    if (text.length == 0) {
+        return;
+    }
+    array2.push(text);
+    display_array2();
+}
+
 function display_array()
 {
     var e = "<hr/>";
@@ -22,10 +33,23 @@ function display_array()
    document.getElementById("Result").innerHTML = e;
 }
 
+function display_array2() {
+    var e = "<hr/>";
+    for (var y = 0; y < array2.length; y++) {
+        e += array2[y] + "<br/>";
+    }
+    document.getElementById("Result2").innerHTML = e;
+}
+
 function clear_array()
 {
     array = [];
     display_array();
+}
+
+function clear_array2() {
+    array2 = [];
+    display_array2();
 }
 
 function solve() {
